12fa2bf17911082c87c1430825a5c50f30b37bd9
Daniel Korsah <dkorsah15@aol.co.uk>
2020-06-25 23:07:33 +0100

    Check settings for max results

--- cogs/Standard.py
+++ cogs/Standard.py
@@ -1,5 +1,6 @@
 from helper_functions import reddit_helpers as rh
 from helper_functions import discord_helpers as dh
+from helper_functions import db_access as db
 
 
 class Standard(dh.commands.Cog):
@@ -10,32 +11,52 @@ class Standard(dh.commands.Cog):
 
     @dh.commands.command(pass_context=True)
     async def hot(self, ctx, subreddit_name, num=5):
-        subreddit = self.reddit.subreddit(subreddit_name)
-        # get get [num] posts from [hot] on [subreddit]
-        hot_list = rh.get_nonsticky_submissions(subreddit, "hot", num)
-        await dh.post_links(ctx, hot_list)
+        settings = db.get_settings(ctx.guild.id)
+
+        if num < settings[0][1]:
+            subreddit = self.reddit.subreddit(subreddit_name)
+            # get get [num] posts from [hot] on [subreddit]
+            hot_list = rh.get_nonsticky_submissions(subreddit, "hot", num)
+            await dh.post_links(ctx, hot_list)
+        else:
+            await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
 
     @dh.commands.command(pass_context=True)
     async def new(self, ctx, subreddit_name, num=5):
-        subreddit = self.reddit.subreddit(subreddit_name)
-        # get get [num] posts from [new] on [subreddit]
-        new_list = rh.get_nonsticky_submissions(subreddit, "new", num)
-        await dh.post_links(ctx, new_list)
+        settings = db.get_settings(ctx.guild.id)
+
+        if num < settings[0][1]:
+            subreddit = self.reddit.subreddit(subreddit_name)
+            # get get [num] posts from [new] on [subreddit]
+            new_list = rh.get_nonsticky_submissions(subreddit, "new", num)
+            await dh.post_links(ctx, new_list)
+        else:
+            await ctx.send(f"This subreddit caps the number of results reuested at {settings[0][1]}")
 
     @dh.commands.command(pass_context=True)
     async def top(self, ctx, subreddit_name, num=5):
-        subreddit = self.reddit.subreddit(subreddit_name)
-        # get get [num] posts from [top] on [subreddit]
-        top_list = rh.get_nonsticky_submissions(subreddit, "top", num)
-        await dh.post_links(ctx, top_list)
+        settings = db.get_settings(ctx.guild.id)
+
+        if num < settings[0][1]:
+            subreddit = self.reddit.subreddit(subreddit_name)
+            # get get [num] posts from [top] on [subreddit]
+            top_list = rh.get_nonsticky_submissions(subreddit, "top", num)
+            await dh.post_links(ctx, top_list)
+        else:
+            await ctx.send(f"This subreddit caps the number of results reuested at {settings[0][1]}")
 
     @dh.commands.command(pass_context=True)
     async def controversial(self, ctx, subreddit_name, num=5):
-        subreddit = self.reddit.subreddit(subreddit_name)
-        # get get [num] posts from [top] on [subreddit]
-        cont_list = rh.get_nonsticky_submissions(
-            subreddit, "controversial", num)
-        await dh.post_links(ctx, cont_list)
+        settings = db.get_settings(ctx.guild.id)
+
+        if num < settings[0][1]:
+            subreddit = self.reddit.subreddit(subreddit_name)
+            # get get [num] posts from [top] on [subreddit]
+            cont_list = rh.get_nonsticky_submissions(
+                subreddit, "controversial", num)
+            await dh.post_links(ctx, cont_list)
+        else:
+            await ctx.send(f"This subreddit caps the number of results reuested at {settings[0][1]}")
 
     @dh.commands.command(pass_context=True)
     async def random(self, ctx, subreddit_name):
