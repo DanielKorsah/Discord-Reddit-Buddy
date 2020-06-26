6a6c69894af394929277d8ee9e7011757c03612f
Daniel Korsah <dkorsah15@aol.co.uk>
2020-06-26 20:19:39 +0100

    Restrict settings commands to admins.

--- cogs/Settings.py
+++ cogs/Settings.py
@@ -16,16 +16,19 @@ class Settings(dh.commands.Cog):
               str(db.get_settings(guild.id)[0]))
 
     @dh.commands.command(pass_context=True)
+    @dh.discord.ext.commands.has_permissions(administrator=True)
     async def change_all_settings(self, ctx, default_results_length, max_results_length, nsfw):
         db.add_settings(ctx.guild.id, default_results_length,
                         max_results_length, nsfw)
         await ctx.send(f"New settings: {db.get_settings(ctx.guild.id)[0]}")
 
     @dh.commands.command(pass_context=True)
+    @dh.discord.ext.commands.has_permissions(administrator=True)
     async def get_settings(self, ctx):
         await ctx.send(db.get_settings(ctx.guild.id)[0])
 
     @dh.commands.command(pass_context=True)
+    @dh.discord.ext.commands.has_permissions(administrator=True)
     async def toggle_nsfw(self, ctx):
         length, max_length, nsfw = db.get_settings(ctx.guild.id)[0]
         nsfw = not nsfw
@@ -33,11 +36,13 @@ class Settings(dh.commands.Cog):
         await ctx.send(f"NSFW Restricted: {bool(db.get_settings(ctx.guild.id)[0][2])}")
 
     @dh.commands.command(pass_context=True)
+    @dh.discord.ext.commands.has_permissions(administrator=True)
     async def set_max_results(self, ctx, maximum_results):
         db.set_max_results(ctx.guild.id, maximum_results)
         await ctx.send(f"New max results set: {db.get_settings(ctx.guild.id)[0][1]}")
 
     @dh.commands.command(pass_context=True)
+    @dh.discord.ext.commands.has_permissions(administrator=True)
     async def set_default_results(self, ctx, default_results_number):
         db.set_default_results(ctx.guild.id, default_results_number)
         await ctx.send(f"New default results length set: {db.get_settings(ctx.guild.id)[0][0]}")
