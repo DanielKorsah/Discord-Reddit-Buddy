c8378e379bfc43032977c0b862be25cafa6068f8
Daniel Korsah <dkorsah15@aol.co.uk>
2020-06-26 19:22:20 +0100

    Refactor hot, new, top, and controversial> All now call one function which takes their names for use in the reddit API call.

--- cogs/Standard.py
+++ cogs/Standard.py
@@ -10,57 +10,24 @@ class Standard(dh.commands.Cog):
         self.bot = bot
 
     @dh.commands.command(pass_context=True)
-    async def hot(self, ctx, subreddit_name, num=5):
-        settings = db.get_settings(ctx.guild.id)
-        num = settings[0][0]
-
-        if num < settings[0][1]:
-            subreddit = self.reddit.subreddit(subreddit_name)
-            # get get [num] posts from [hot] on [subreddit]
-            hot_list = rh.get_nonsticky_submissions(subreddit, "hot", num)
-            await dh.post_links(ctx, hot_list)
-        else:
-            await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
+    async def hot(self, ctx, subreddit_name, number_of_results=None):
+        await dh.print_reddit_results(ctx, subreddit_name,
+                                      self.reddit, number_of_results)
 
     @dh.commands.command(pass_context=True)
-    async def new(self, ctx, subreddit_name, num=5):
-        settings = db.get_settings(ctx.guild.id)
-        num = settings[0][0]
-
-        if num < settings[0][1]:
-            subreddit = self.reddit.subreddit(subreddit_name)
-            # get get [num] posts from [new] on [subreddit]
-            new_list = rh.get_nonsticky_submissions(subreddit, "new", num)
-            await dh.post_links(ctx, new_list)
-        else:
-            await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
+    async def new(self, ctx, subreddit_name, number_of_results=None):
+        await dh.print_reddit_results(ctx, subreddit_name,
+                                      self.reddit, number_of_results)
 
     @dh.commands.command(pass_context=True)
-    async def top(self, ctx, subreddit_name, num=5):
-        settings = db.get_settings(ctx.guild.id)
-        num = settings[0][0]
-
-        if num < settings[0][1]:
-            subreddit = self.reddit.subreddit(subreddit_name)
-            # get get [num] posts from [top] on [subreddit]
-            top_list = rh.get_nonsticky_submissions(subreddit, "top", num)
-            await dh.post_links(ctx, top_list)
-        else:
-            await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
+    async def top(self, ctx, subreddit_name, number_of_results=None):
+        await dh.print_reddit_results(ctx, subreddit_name,
+                                      self.reddit, number_of_results)
 
     @dh.commands.command(pass_context=True)
-    async def controversial(self, ctx, subreddit_name, num=5):
-        settings = db.get_settings(ctx.guild.id)
-        num = settings[0][0]
-
-        if num < settings[0][1]:
-            subreddit = self.reddit.subreddit(subreddit_name)
-            # get get [num] posts from [top] on [subreddit]
-            cont_list = rh.get_nonsticky_submissions(
-                subreddit, "controversial", num)
-            await dh.post_links(ctx, cont_list)
-        else:
-            await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
+    async def controversial(self, ctx, subreddit_name, number_of_results=None):
+        await dh.print_reddit_results(ctx, subreddit_name,
+                                      self.reddit, number_of_results)
 
     @dh.commands.command(pass_context=True)
     async def random(self, ctx, subreddit_name):
--- helper_functions/discord_helpers.py
+++ helper_functions/discord_helpers.py
@@ -1,6 +1,9 @@
+import inspect
 import discord
 from discord.ext import commands
 from discord.ext.commands import bot
+from helper_functions import db_access as db
+from helper_functions import reddit_helpers as rh
 
 
 def discord_auth():
@@ -14,3 +17,23 @@ def discord_auth():
 async def post_links(ctx, posts):
     for post in posts:
         await ctx.send(post.title + "\n" + post.url)
+
+
+async def print_reddit_results(ctx, subreddit_name, reddit, num):
+    settings = db.get_settings(ctx.guild.id)
+
+    # if no results length given use server default
+    if (num == None):
+        num = settings[0][0]
+
+    if num < settings[0][1]:
+        subreddit = reddit.subreddit(subreddit_name)
+
+        # sort type is the name of the calling fucntion as a string
+        sort_type = inspect.stack()[1][3]
+        # get get [num] posts from [sort_type] on [subreddit]
+        result_list = rh.get_nonsticky_submissions(
+            subreddit, f"{sort_type}", num)
+        await post_links(ctx, result_list)
+    else:
+        await ctx.send(f"This server caps the number of results requested at {settings[0][1]}")
