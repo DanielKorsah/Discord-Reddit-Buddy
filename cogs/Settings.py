df0cad1ba58b200554506c1596cced56c2a67aef
Daniel Korsah <dkorsah15@aol.co.uk>
2020-06-26 20:02:13 +0100

    User feedback on settings updates.

--- cogs/Settings.py
+++ cogs/Settings.py
@@ -19,6 +19,7 @@ class Settings(dh.commands.Cog):
     async def change_all_settings(self, ctx, default_results_length, max_results_length, nsfw):
         db.add_settings(ctx.guild.id, default_results_length,
                         max_results_length, nsfw)
+        await ctx.send(f"New settings: {db.get_settings(ctx.guild.id)[0]}")
 
     @dh.commands.command(pass_context=True)
     async def print_settings(self, ctx):
@@ -29,18 +30,17 @@ class Settings(dh.commands.Cog):
         length, max_length, nsfw = db.get_settings(ctx.guild.id)[0]
         nsfw = not nsfw
         db.add_settings(ctx.guild.id, length, max_length, nsfw)
-        await ctx.send("New settings")
-        await ctx.send(db.get_settings(ctx.guild.id)[0])
+        await ctx.send(f"NSFW Restricted: {bool(db.get_settings(ctx.guild.id)[0][2])}")
 
     @dh.commands.command(pass_context=True)
     async def set_max_results(self, ctx, maximum_results):
         db.set_max_results(ctx.guild.id, maximum_results)
-        await ctx.send("updated")
+        await ctx.send(f"New max results set: {db.get_settings(ctx.guild.id)[0][1]}")
 
     @dh.commands.command(pass_context=True)
     async def set_default_results(self, ctx, default_results_number):
         db.set_default_results(ctx.guild.id, default_results_number)
-        await ctx.send("updated")
+        await ctx.send(f"New default results length set: {db.get_settings(ctx.guild.id)[0][0]}")
 
 
 def setup(bot):
