764b35a4959988f8b58bfba466125947ed33562f
DanielKorsah <dkorsah15@aol.co.uk>
2021-12-02 02:37:29 +0000

    find number of active servers

--- RedditBuddy.py
+++ RedditBuddy.py
@@ -21,6 +21,7 @@ async def on_ready():
 
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("Praise Geraldo"))
     print("Reddit Buddy online!")
+    print(f"active in {len(bot.guilds)} servers")
 
 # authenticate and run discord bot
 bot.run(dh.discord_auth())
